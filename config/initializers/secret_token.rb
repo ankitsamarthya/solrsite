e6ac26c1f4b1d313a384c82a6f1a70c64e1c4b10
Shubham <shubham.kedia2010@gmail.com>
2014-02-22 17:42:05 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Solrsite::Application.config.secret_key_base = 'd06a1b1214a4a8c00961e939211c6ae9a5b31e03e10b461d15ab62c1c5839ad4dcabb207190c060c7bf42e66c4925239918844d5c3e62b622b4665a16704ea68'
